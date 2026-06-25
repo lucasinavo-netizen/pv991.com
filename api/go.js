e401d63e4f8ca64023e93dc2b5c627c71c50d136
lucasinavo-netizen <lucasinavo@gmail.com>
2026-06-25 16:52:17 +0800

    seo: use RR95K as sole affiliate redirect

--- api/go.js
+++ api/go.js
@@ -1,8 +1,6 @@
-const A = 'https://www.rr95k.com/?ch=0cf28df51e';
-const B = 'https://www.mm38aa.com/m/home?affiliateCode=lu0001';
+const TARGET = 'https://www.rr95k.com/?ch=0cf28df51e';
 
 export default function handler(req, res) {
-  const target = req.query.to === 'mm38' ? B : A;
   res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
-  res.redirect(302, target);
+  res.redirect(302, TARGET);
 }
